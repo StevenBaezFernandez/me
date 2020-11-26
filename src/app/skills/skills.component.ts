7d0a94cac75943c0e85968f960fe0cff5ec1f9b8
Steven <stevenbaezfernandez@gmail.com>
2020-11-26 16:37:31 -0500

    upgrading about and skills pages

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { NgxSpinnerService } from "ngx-spinner";
 
+import * as Typed from 'typed.js';
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
@@ -14,11 +16,25 @@ export class SkillsComponent implements OnInit {
   ngOnInit() {
     /** spinner starts on init */
     this.spinnerSevice.show();
- 
+
     setTimeout(() => {
       /** spinner ends after 5 seconds */
       this.spinnerSevice.hide();
     }, 1000);
-  }
+
+    const options = {
+     strings: ["<span>About me.</span>"],
+     typeSpeed: 70,
+     backSpeed: 100,
+     showCursor: true,
+     cursorChar: '|',
+     loop: false
+   };
+ 
+   setTimeout(() => {
+     const typed = new Typed.default('.typed', options);
+   
+   }, 1300);
+ }
 
 }
